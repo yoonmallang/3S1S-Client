6525be11969e5f502bf9a652cb002a3a5f90f2f0
Woosung Kim <woosung0420k@naver.com>
2021-11-10 22:00:25 +0900

    프로젝트 리스트 페이지 UI

--- ssis/src/components/project/List.js
+++ ssis/src/components/project/List.js
@@ -1,11 +1,97 @@
 import React, { Component } from 'react';
+import '../../css/project/project.css';
+import { Stack } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class List extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isLogin : false,
+            needMiddleBar : false
+        }
+    }
     render() {
         return (
-            <p>
-                project list
-            </p>
+            <div className = "Project">
+                <div className = "ProjectContent">
+                    
+                    <div className = "ProjectBox">
+                        <div className = "Title">
+                            OSS 공모전
+                        </div>
+                        <button type="button" class="btm" id="img_btn"><img src="img/pencil.png" class="btm_image" ></img></button>
+                        <button type="button" class="btm" id="img_btn"><img src="img/cancel.png" class="btm_image" ></img></button>
+                        <div className = "ImgTeam">
+                            <div className = "Image">
+                                <img src = "img/group.png" className = "Img" alt = "img/group.png"></img>
+                            </div>
+                            <div className = "Team">
+                                3s1s
+                            </div>
+                        </div>
+                        <progress value="22" max="100" className="ProgressBar"></progress>
+                    </div>
+                    <div className = "ProjectBox">
+                        <div className = "Title">
+                            소공 플젝
+                        </div>
+                        <button type="button" class="btm" id="img_btn"><img src="img/pencil.png" class="btm_image" ></img></button>
+                        <button type="button" class="btm" id="img_btn"><img src="img/cancel.png" class="btm_image" ></img></button>
+                        <div className = "ImgTeam">
+                            <div className = "Image">
+                                <img src = "img/group.png" className = "Img" alt = "img/group.png"></img>
+                            </div>
+                            <div className = "Team">
+                                SSIS
+                            </div>
+                        </div>
+                        <progress value="22" max="100" className="ProgressBar"></progress>
+                    </div>
+                    <div className = "ProjectBox">
+                        <div className = "Title">
+                            쏘공 스터디
+                        </div>
+                        <button type="button" class="btm" id="img_btn"><img src="img/pencil.png" class="btm_image" ></img></button>
+                        <button type="button" class="btm" id="img_btn"><img src="img/cancel.png" class="btm_image" ></img></button>
+                        <div className = "ImgTeam">
+                            <div className = "Image">
+                                <img src = "img/group.png" className = "Img" alt = "img/group.png"></img>
+                            </div>
+                            <div className = "Team">
+                                삼성일식
+                            </div>
+                        </div>
+                        <progress value="22" max="100" className="ProgressBar"></progress>
+                    </div>
+                    <div className = "ProjectBox">
+                        <div className = "Title">
+                            알고리즘 스터디
+                        </div>
+                        <button type="button" class="btm" id="img_btn"><img src="img/pencil.png" class="btm_image" ></img></button>
+                        <button type="button" class="btm" id="img_btn"><img src="img/cancel.png" class="btm_image" ></img></button>
+                        <div className = "ImgTeam">
+                            <div className = "Image">
+                                <img src = "img/group.png" className = "Img" alt = "img/group.png"></img>
+                            </div>
+                            <div className = "Team">
+                                삼성일식
+                            </div>
+                        </div>
+                        <progress value="22" max="100" className="ProgressBar"></progress>
+                    </div>
+                        
+                        
+                    
+                    <div className = "AddProject">
+                        <button type="button" class="btm_add" id="img_btn"><img src="img/plus.png" class="btm_image" ></img></button>
+                        
+                    </div>
+                    <p>
+                    project list
+                    </p>
+                </div>
+            </div>
         );
     }
 }
