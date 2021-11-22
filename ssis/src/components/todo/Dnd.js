f1ca96efaa7944a2234802802ffc42f887a6a701
minnseong <kdsvip5@naver.com>
2021-11-22 21:56:58 +0900

    ToDo 드래그 앤 드랍시 상태변경

--- ssis/src/components/todo/Dnd.js
+++ ssis/src/components/todo/Dnd.js
@@ -1,133 +1,122 @@
+import axios from "axios";
 import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import {v4 as uuidv4} from 'uuid';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/todo/list.css';
 
-const itemsFromBackend = [
-  { id: uuidv4(), content: "First task" },
-  { id: uuidv4(), content: "Second task" },
-  { id: uuidv4(), content: "Third task" },
-  { id: uuidv4(), content: "Fourth task" },
-  { id: uuidv4(), content: "Fifth task" }
-];
-
-const columnsFromBackend = {
-  [uuidv4()]: {
-    name: "시작전",
-    items: itemsFromBackend
-  },
-  [uuidv4()]: {
-    name: "진행중",
-    items: []
-  },
-  [uuidv4()]: {
-    name: "완료",
-    items: []
-  }
-};
-
-const onDragEnd = (result, columns, setColumns) => {
-  if (!result.destination) return;
-  const { source, destination } = result;
-
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId];
-    const destColumn = columns[destination.droppableId];
-    const sourceItems = [...sourceColumn.items];
-    const destItems = [...destColumn.items];
-    const [removed] = sourceItems.splice(source.index, 1);
-    destItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems
-      }
-    });
-  } else {
-    const column = columns[source.droppableId];
-    const copiedItems = [...column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems
-      }
-    });
-  }
-};
-
 function Dnd(props) {
+  const [columns, setColumns] = useState({
+    "0": {
+      name: "시작2전",
+      items: props.state0,
+      state : 0
+    },
+    "1": {
+      name: "진행중",
+      items: props.state1,
+      state : 1
+    },
+    "2": {
+      name: "완료",
+      items: props.state2,
+      state : 2
+    }
+  });
 
-console.log(props.state0)
-console.log(props.state1)
-console.log(props.state2)
-  const [columns, setColumns] = useState(columnsFromBackend);
+  const onDragEnd = (result, columns, setColumns) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+  
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+
+      axios.post('http://ec2-3-34-73-102.ap-northeast-2.compute.amazonaws.com/todos/change',{
+        'todo' : [removed][0]['id'],
+        'state' : destColumn['state']
+      })
+
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems
+        }
+      });
+    } 
+    else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems
+        }
+      });
+    }
+  };
+  
+  const titleClassName = ['p1', 'p2', 'p3']
   return (
     <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
-      <DragDropContext
-        onDragEnd={result => onDragEnd(result, columns, setColumns)}
-      >
+      <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
         {Object.entries(columns).map(([columnId, column], index) => {
           return (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center"
-              }}
-              key={columnId}
-            >
-              <h2>{column.name}</h2>
-              <div style={{ margin: 8 }}>
+            <div  style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center"
+            }} key={columnId}>
+              <span className={titleClassName[index]}>{column.name}</span>
+              <div style={{ marginLeft : 70, marginRight :70, marginTop:30}}>
                 <Droppable droppableId={columnId} key={columnId}>
                   {(provided, snapshot) => {
                     return (
-                      <div
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                        style={{
-                          background: snapshot.isDraggingOver
-                            ? "lightblue"
-                            : "lightgrey",
-                          padding: 4,
-                          width: 250,
-                          minHeight: 500
-                        }}
-                      >
+                      <div style={{
+                        padding: 4,
+                        width: 280,
+                      }}{...provided.droppableProps} ref={provided.innerRef}> 
                         {column.items.map((item, index) => {
                           return (
-                            <Draggable
-                              key={item.id}
-                              draggableId={item.id}
-                              index={index}
-                            >
+                            <Draggable key={item.id} draggableId={item.id} index={index}>
                               {(provided, snapshot) => {
                                 return (
-                                  <div
-                                    ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}
-                                    style={{
-                                      userSelect: "none",
-                                      padding: 16,
-                                      margin: "0 0 8px 0",
-                                      minHeight: "50px",
-                                      backgroundColor: snapshot.isDragging
-                                        ? "#263B4A"
-                                        : "#456C86",
-                                      color: "white",
-                                      ...provided.draggableProps.style
-                                    }}
-                                  >
-                                    {item.content}
-                                  </div>
+                                  <Card ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}  
+                                  style={{
+                                    userSelect: "none",
+                                    padding: 2,
+                                    margin: "0 0 12px 0",
+                                    minHeight: "130px",  
+                                    ...provided.draggableProps.style
+                                  }}>
+                                    <Card.Body>
+                                        <div className ="one-line">
+                                            <Card.Title className = "todo-title">{item.title}</Card.Title>
+                                            <span className = "todo-dday">{item.d_day}</span>
+                                        </div>
+                                        <div className ="participants">
+                                            {
+                                                item.participants.map((member)=> {
+                                                    return (
+                                                        <Card.Text key={member} className ="participant">{member}</Card.Text>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                    </Card.Body>
+                                  </Card>
                                 );
                               }}
                             </Draggable>
--- ssis/src/components/todo/List.js
+++ ssis/src/components/todo/List.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import '../../css/todo/list.css';
-import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import Create from './Create.js'
@@ -67,100 +66,27 @@ class List extends Component {
     componentDidMount() { 
         const { loadingTodos } = this; 
         loadingTodos(); 
+        
     }
     render() {
         const todos_0 = this.state.todos_0
         const todos_1 = this.state.todos_1
         const todos_2 = this.state.todos_2
-
+        
         return (
             <div className = "todo-page">
                 <Middlebar id={this.props.match.params}/>
                     <div style={{ width: '1100px', margin: '0px auto', marginTop:'30px'}}>
-                    <Create id={this.props.match.params}/>
-                    <div style={{ width: '500px',margin: '0px auto'}}>
-                    <p style={{ float: 'left', marginBottom:'0px'}}>진행률</p>
-                    <p style={{ float: 'right', marginBottom:'0px'}}>5%</p>
-                    <progress value = '5' max="100" className="todo-progress"></progress>
+                        <Create id={this.props.match.params}/>
+                        <div style={{ width: '500px',margin: '0px auto'}}>
+                        <p style={{ float: 'left', marginBottom:'0px'}}>진행률</p>
+                        <p style={{ float: 'right', marginBottom:'0px'}}>5%</p>
+                        <progress value = '5' max="100" className="todo-progress"></progress>
                     </div>
                 </div>
                 <div>
                 <Dnd state0={todos_0} state1={todos_1} state2={todos_2}/>
                 </div>
-                {/* <div className = "todoList">
-                    <div className = "state left">
-                        <span className="p1">시작전</span>
-                        {todos_0.map((item)=> {
-                            return (
-                                <Card className = "todo-card" key = {item.id} style={{ width: '18rem'}}>
-                                    <Card.Body>
-                                        <div className ="one-line">
-                                            <Card.Title className = "todo-title">{item.title}</Card.Title>
-                                            <span className = "todo-dday">{item.d_day}</span>
-                                        </div>
-                                        <div className ="participants">
-                                            {
-                                                item.participants.map((member)=> {
-                                                    return (
-                                                        <Card.Text key={member} className ="participant">{member}</Card.Text>
-                                                    )
-                                                })
-                                            }
-                                        </div>
-                                    </Card.Body>
-                                </Card>
-                            )
-                        })}
-                    </div>
-                    <div className = "state center">
-                        <span className="p2">진행중</span>
-                        {todos_1.map((item)=> {
-                                return (
-                                    <Card className = "todo-card" key = {item.id} style={{ width: '18rem'}}>
-                                        <Card.Body>
-                                            <div className ="one-line">
-                                                <Card.Title className = "todo-title">{item.title}</Card.Title>
-                                                <span className = "todo-dday">{item.d_day}</span>
-                                            </div>
-                                            <div className ="participants">
-                                                {
-                                                    item.participants.map((member)=> {
-                                                        return (
-                                                            <Card.Text key={member} className ="participant">{member}</Card.Text>
-                                                        )
-                                                    })
-                                                }
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
-                                )
-                            })} 
-                    </div>
-                    <div className = "state right">
-                        <span className="p3">완료</span>
-                        {todos_2.map((item)=> {
-                                return (
-                                    <Card className = "todo-card" key = {item.id} style={{ width: '18rem'}}>
-                                        <Card.Body>
-                                            <div className ="one-line">
-                                                <Card.Title className = "todo-title">{item.title}</Card.Title>
-                                                <span className = "todo-dday">{item.d_day}</span>
-                                            </div>
-                                            <div className ="participants">
-                                                {
-                                                    item.participants.map((member)=> {
-                                                        return (
-                                                            <Card.Text key={member} className ="participant">{member}</Card.Text>
-                                                        )
-                                                    })
-                                                }
-                                            </div>
-                                        </Card.Body>
-                                    </Card>
-                                )
-                            })}
-                    </div>
-                </div>     */}
             </div>
         );
     }
